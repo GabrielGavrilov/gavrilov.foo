218e40778de15ab078415e2118a251b7509838d0
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-08-20 10:24:20 -0400

    Update

--- src/features/projects/project-card.tsx
+++ src/features/projects/project-card.tsx
@@ -1,9 +1,22 @@
 interface Props {
   title: string;
   description: string;
-  languages: string;
+  languages?: string;
 }
 
 export default function ProjectCard({ title, description, languages }: Props) {
-  return <div>ll</div>;
+  function extractLanguages() {
+    return languages?.split(',');
+  }
+
+  return (
+    <div className="w-[330px] h-[425px] shadow-lg bg-white">
+      <div className="w-full h-1/2 bg-gray-300"></div>
+      <div className="my-2 p-2">
+        <p className="text-2xl">{title}</p>
+        <p className="mt-2">{description}</p>
+      </div>
+      <div></div>
+    </div>
+  );
 }
--- src/features/projects/projects.tsx
+++ src/features/projects/projects.tsx
@@ -1,7 +1,16 @@
+import ProjectCard from './project-card';
+
 export default function Projects() {
   return (
-    <div className="w-full h-full bg-slate-50">
-      <p>Projects</p>
+    <div className="w-full h-full bg-slate-50 flex justify-center">
+      <div className="w-8/12 p-6 grid grid-cols-3 gap-3">
+        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+      </div>
     </div>
   );
 }
