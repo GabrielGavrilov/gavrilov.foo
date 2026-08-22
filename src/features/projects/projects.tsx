e3cec78e2583f99bb69886ccae5b505c9e3ba030
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-08-21 20:31:02 -0400

    Update

--- src/features/projects/projects.tsx
+++ src/features/projects/projects.tsx
@@ -4,13 +4,15 @@ export default function Projects() {
   return (
     <div className="w-full h-full flex justify-center">
       <div className="w-8/12 p-6 grid grid-cols-3 gap-3">
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
-        <ProjectCard title="Mocha" description="Lorem Ipsum Dolor" />
+        <ProjectCard title="Kumi" description="Lorem Ipsum Dolor" />
+        <ProjectCard
+          title="Mocha"
+          description="A light-weight opinionated microservice framework built from scratch using TCP"
+        />
+        <ProjectCard
+          title="Macchiato"
+          description="An object-relational mapping library for Java with JPA-like syntax"
+        />
       </div>
     </div>
   );
