9ce315072ef267208ac079feb742aba9a8e15e64
Gabriel Gavrilov <gabriel.gavrilov02@gmail.com>
2026-09-14 09:32:42 -0400

    Update

--- src/features/projects/project-card.tsx
+++ src/features/projects/project-card.tsx
@@ -1,7 +1,7 @@
 interface Props {
   title: string;
   description: string;
-  languages?: string;
+  languages: string;
 }
 
 export default function ProjectCard({ title, description, languages }: Props) {
@@ -15,8 +15,12 @@ export default function ProjectCard({ title, description, languages }: Props) {
       <div className="my-2 p-2">
         <p className="text-2xl">{title}</p>
         <p className="mt-2">{description}</p>
+        <span className="flex mt-4">
+          {extractLanguages().map((language) => (
+            <p className="px-3 mr-2 bg-gray-200 rounded-xl">{language}</p>
+          ))}
+        </span>
       </div>
-      <div></div>
     </div>
   );
 }
--- src/features/projects/projects.tsx
+++ src/features/projects/projects.tsx
@@ -4,14 +4,20 @@ export default function Projects() {
   return (
     <div className="w-full h-full flex justify-center">
       <div className="w-8/12 p-6 grid grid-cols-3 gap-3">
-        <ProjectCard title="Kumi" description="Lorem Ipsum Dolor" />
+        <ProjectCard
+          title="Kumi"
+          description="An AI agent orchestration platform with custom tool calling capabilities through the use of HTTP"
+          languages="C#,.NET,TypeScript,React"
+        />
         <ProjectCard
           title="Mocha"
           description="A light-weight opinionated microservice framework built from scratch using TCP"
+          languages="Java"
         />
         <ProjectCard
           title="Macchiato"
-          description="An object-relational mapping library for Java with JPA-like syntax"
+          description="An object-relational mapping library with JPA-like syntax for Java"
+          languages="Java"
         />
       </div>
     </div>
